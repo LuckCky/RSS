ed961a3720a5d4b41a390672c52ea0c143c0858e
User <User>
2016-07-18 15:56:40 +0300

    rss_page is working

--- lenta_rss/lenta/lenta/urls.py
+++ lenta_rss/lenta/lenta/urls.py
@@ -15,9 +15,10 @@ Including another URLconf
 """
 from django.conf.urls import url
 from django.contrib import admin
+from lenta.views import hello
 
 urlpatterns = [
     url(r'^admin/', admin.site.urls),
-    url(r'', hello),
+    url(r'^rss/', hello),
 
 ]
