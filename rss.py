ec3664788640f3ca4a4a170062796146c818f61a
LuckCky <www.aivanov@gmail.com>
2016-07-26 23:58:42 +0300

    db ascending

--- rss.py
+++ rss.py
@@ -2,14 +2,10 @@ from celery import Celery
 import feedparser
 
 app = Celery('rss', broker='amqp://guest@localhost//')
-app.conf.CELERY_RESULT_BACKEND = 'db+sqlite:///results.sqlite'
+app.conf.CELERY_RESULT_BACKEND = 'db+sqlite:///results.sqlite3'
 
 @app.task
 def feeds(x):
     feed = feedparser.parse(x)
     # feed = str(feed).encode('utf8')
     return feed
-
-@app.task
-def add(x, y):
-    return x + y
