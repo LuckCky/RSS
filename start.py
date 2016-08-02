cc8e2916727f794c6d1a8c25599dc50b5cafab4f
LuckCky <www.aivanov@gmail.com>
2016-08-03 01:09:17 +0300

    date needs to be fixed

--- start.py
+++ start.py
@@ -3,17 +3,24 @@ from lenta_rss.lenta.lrss.models import News
 
 result = tasks.feeds.delay('https://lenta.ru/rss')
 c = result.get(timeout=5)
-for x in range(0, len(c['entries'])):
-    #проверить есть ли c['entries'][x]['title'] в БД, если нет, то записать
-    try:
-        News.objects.get(name=c['entries'][x]['title'])
-        pass
-    except News.DoesNotExist:
-        #add entry to DB
-        print(x)
+# for x in range(0, len(c['entries'])):
+#     try:
+#         News.objects.get(name=c['entries'][x]['title'])
+#         pass
+#     except News.DoesNotExist:
+#         n = News(link=c['entries'][x]['link'],
+#                  name=c['entries'][x]['title'],
+#                  date=c['entries'][x]['published'],
+#                  rubric=c['entries'][x]['tags'][0]['term'],
+#                  brief=c['entries'][x]['summary'])
+#         n.save()
 
-    # print('link:', c['entries'][x]['link'])
-    # print('title:', c['entries'][x]['title'])
-    # print('date:', c['entries'][x]['published'])
-    # print('tags:', c['entries'][x]['tags'][0]['term'])
-    # print('-'*20)
+
+for x in range(0, len(c['entries'])):
+#     print('link:', c['entries'][x]['link'])
+#     print('title:', c['entries'][x]['title'])
+    print('date:', c['entries'][x]['published'])
+#     print('tags:', c['entries'][x]['tags'][0]['term'])
+#     print('brief:', c['entries'][x]['summary'])
+#     print('-'*20)
+# print(c['entries'])
