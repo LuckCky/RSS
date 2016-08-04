5748e3c172050789ef810eb48ba78a2ba2f73cf2
LuckCky <www.aivanov@gmail.com>
2016-08-05 00:24:32 +0300

    rss adds to db

--- start.py
+++ start.py
@@ -1,26 +1,35 @@
 import tasks
 from lenta_rss.lenta.lrss.models import News
+import datetime
 
 result = tasks.feeds.delay('https://lenta.ru/rss')
 c = result.get(timeout=5)
-# for x in range(0, len(c['entries'])):
-#     try:
-#         News.objects.get(name=c['entries'][x]['title'])
-#         pass
-#     except News.DoesNotExist:
-#         n = News(link=c['entries'][x]['link'],
-#                  name=c['entries'][x]['title'],
-#                  date=c['entries'][x]['published'],
-#                  rubric=c['entries'][x]['tags'][0]['term'],
-#                  brief=c['entries'][x]['summary'])
-#         n.save()
-
-
 for x in range(0, len(c['entries'])):
-#     print('link:', c['entries'][x]['link'])
-#     print('title:', c['entries'][x]['title'])
-    print('date:', c['entries'][x]['published'])
-#     print('tags:', c['entries'][x]['tags'][0]['term'])
-#     print('brief:', c['entries'][x]['summary'])
-#     print('-'*20)
-# print(c['entries'])
+    try:
+        News.objects.get(name=c['entries'][x]['title'])
+        pass
+    except News.DoesNotExist:
+        date = c['entries'][x]['published'].split(',')
+        date = date[1].split(' ')
+        day = int(date[1])
+        months = {'Jan': 1,
+                  'Feb': 2,
+                  'Mar': 3,
+                  'Apr': 4,
+                  'May': 5,
+                  'Jun': 6,
+                  'Jul': 7,
+                  'Aug': 8,
+                  'Sep': 9,
+                  'Oct': 10,
+                  'Nov': 11,
+                  'Dec': 12}
+        month = int(months[date[2]])
+        year = int(date[3])
+        date = datetime.date(year, month, day)
+        n = News(link=c['entries'][x]['link'],
+                 name=c['entries'][x]['title'],
+                 date=date,
+                 rubric=c['entries'][x]['tags'][0]['term'],
+                 brief=c['entries'][x]['summary'])
+        n.save()
