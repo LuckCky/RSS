62977311fcba9de655e9bca575dec429f8658ffc
User <User>
2016-08-11 19:13:29 +0300

    html

--- lenta_rss/lenta/lrss/views.py
+++ lenta_rss/lenta/lrss/views.py
@@ -9,7 +9,7 @@ def search(request):#from and till
     if 'from' in request.GET and request.GET['from'] and 'till' in request.GET and request.GET['till']:
         f = request.GET['from']
         t = request.GET['till']
-        news = Book.objects.filter(date__icontains=f)
+        news = Book.objects.filter(Date__icontains=f)
         return render_to_response('search_results.html', {'News': news, 'query': f, 'query2': t})
     else:
         return HttpResponse('Введите поисковый запрос.')
