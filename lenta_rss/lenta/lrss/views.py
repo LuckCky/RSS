92b3474b7e25ad71e7a363846ec49dd58fd4ee89
User <User>
2016-08-11 16:43:50 +0300

    search form

--- lenta_rss/lenta/lrss/views.py
+++ lenta_rss/lenta/lrss/views.py
@@ -1,12 +1,15 @@
 from django.shortcuts import render_to_response
 from django.http import HttpResponse
+from lenta_rss.lenta.lrss.models import News
 
 def search_form(request):
     return render_to_response('search_form.html')
 
-def search(request):
-    if 'q' in request.GET:
-        message = 'Вы искали: {0}'.format(request.GET['q'])
+def search(request):#from and till
+    if 'from' in request.GET and request.GET['from'] and 'till' in request.GET and request.GET['till']:
+        f = request.GET['from']
+        t = request.GET['till']
+        news = Book.objects.filter(date__icontains=f)
+        return render_to_response('search_results.html', {'News': news, 'query': f, 'query2': t})
     else:
-        message = 'Вы отправили пустую форму!'
-    return HttpResponse(message)
+        return HttpResponse('Введите поисковый запрос.')
