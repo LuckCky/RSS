499ef6154e16bff001cef7c8951b8cf937bcae0a
LuckCky <www.aivanov@gmail.com>
2016-08-13 00:39:00 +0300

    html ok, but no proper list

--- lenta_rss/lenta/lrss/views.py
+++ lenta_rss/lenta/lrss/views.py
@@ -1,16 +1,16 @@
 from django.shortcuts import render_to_response
 from django.http import HttpResponse
 from lenta_rss.lenta.lrss.models import News
+import datetime
 
 def search_form(request):
     return render_to_response('search_form.html')
 
-def search(request):#from and till
+def search(request):
     if 'from' in request.GET and request.GET['from'] and 'till' in request.GET and request.GET['till']:
         f = request.GET['from']
         t = request.GET['till']
-        news = News.exclude(Date__gte=datetime.datetime(t))
-        news = news.filter(Date__gte=datetime.datetime(f))
-        return render_to_response('search_results.html', {'News': news, 'query': f, 'query2': t})
+        news = News.objects.filter(date__range=[f, t])
+        return render_to_response('search_results.html', {'News': news, 'from': f, 'till': t})
     else:
         return HttpResponse('Введите поисковый запрос.')
