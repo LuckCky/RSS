16eb6c77966b69a2bfccf917c63491e225a02b3b
LuckCky <www.aivanov@gmail.com>
2016-08-13 20:03:28 +0300

    more news

--- lenta_rss/lenta/lrss/models.py
+++ lenta_rss/lenta/lrss/models.py
@@ -1,11 +1,11 @@
 from django.db import models
 
 class News(models.Model):
+    def __unicode__(self):
+        return self.name
+
     date = models.DateField()
     name = models.TextField()
     brief = models.TextField()
     rubric = models.TextField()
     link = models.TextField()
-
-    def __unicode__(self):
-        return self.name
