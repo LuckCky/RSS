0d64fddb269438c72ff84c24a88b3642b9cf5466
LuckCky <www.aivanov@gmail.com>
2016-08-14 02:04:56 +0300

    html works

--- lenta_rss/lenta/lenta/settings.py
+++ lenta_rss/lenta/lenta/settings.py
@@ -31,6 +31,7 @@ ALLOWED_HOSTS = []
 # Application definition
 
 INSTALLED_APPS = [
+    # 'lrss.apps.LrssConfig',
     'django.contrib.admin',
     'django.contrib.auth',
     'django.contrib.contenttypes',
--- lenta_rss/lenta/lrss/models.py
+++ lenta_rss/lenta/lrss/models.py
@@ -1,11 +1,12 @@
 from django.db import models
 
 class News(models.Model):
-    def __unicode__(self):
-        return self.name
 
-    date = models.DateField()
-    name = models.TextField()
+    date = models.DateField('date published')
+    title = models.CharField(max_length=200)
     brief = models.TextField()
     rubric = models.TextField()
     link = models.TextField()
+
+    def __str__(self):
+        return self.title
