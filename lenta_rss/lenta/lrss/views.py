4e21bfff92d2d16c7d79d0f85566c28961805d02
User <User>
2016-08-16 19:07:28 +0300

    adding rubric to selector

--- lenta_rss/lenta/lrss/views.py
+++ lenta_rss/lenta/lrss/views.py
@@ -3,8 +3,8 @@ from django.http import HttpResponse
 from lenta_rss.lenta.lrss.models import News
 
 def search_form(request):
-    # rubrics = News.objects.filter()
-    return render_to_response('search_form.html')
+    rubrics = News.objects.values('rubric').distinct()
+    return render_to_response('search_form.html', rubrics)
 
 def search(request):
     if 'from' in request.GET and request.GET['from'] and 'till' in request.GET and request.GET['till']:
@@ -14,3 +14,9 @@ def search(request):
         return render_to_response('search_results.html', {'News': news, 'from': f, 'till': t})
     else:
         return HttpResponse('Введите поисковый запрос.')
+
+# if __name__ == '__main__':
+#     from django.conf import settings
+#     settings.configure()
+#     rubrics = News.objects.values('rubric').distinct()
+#     print(rubrics)
