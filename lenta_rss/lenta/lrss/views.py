aed3bc4e261d1c7565c6cadc46bf39a5ac0bbaf7
User <User>
2016-08-17 18:52:40 +0300

    almost rubric

--- lenta_rss/lenta/lrss/views.py
+++ lenta_rss/lenta/lrss/views.py
@@ -4,19 +4,17 @@ from lenta_rss.lenta.lrss.models import News
 
 def search_form(request):
     rubrics = News.objects.values('rubric').distinct()
-    return render_to_response('search_form.html', rubrics)
+    return render_to_response('search_form.html', {'Rubrics': rubrics})
 
 def search(request):
     if 'from' in request.GET and request.GET['from'] and 'till' in request.GET and request.GET['till']:
         f = request.GET['from']
         t = request.GET['till']
-        news = News.objects.filter(date__range=[f, t])
+        if request.GET.get('rubric', None):
+            rubric = request.GET['rubric']
+            news = News.objects.filter(date__range=[f, t], rubric=rubric)
+        else:
+            news = News.objects.filter(date__range=[f, t])
         return render_to_response('search_results.html', {'News': news, 'from': f, 'till': t})
     else:
         return HttpResponse('Введите поисковый запрос.')
-
-# if __name__ == '__main__':
-#     from django.conf import settings
-#     settings.configure()
-#     rubrics = News.objects.values('rubric').distinct()
-#     print(rubrics)
